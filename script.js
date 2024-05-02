1558053b32b362b2aa67d93001e609ca99c9b20f
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-05-02 14:43:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 // your code here
+let heading = document.getElementById("url");
+let nameInput = document.getElementById("name");
+let yearInput = document.getElementById("year");
+let btn = document.getElementById("button");
+
+btn.addEventListener("click",function (e){
+	e.preventDefault();
+	let nameInputValue = nameInput.value;
+	let yearInputValue = yearInput.value;
+	let headingValue = heading.innerText;
+    if (nameInputValue =="" && yearInputValue =="") {
+		heading.innerText =`${headingValue}`;
+	}
+	else if (nameInputValue =="" && yearInputValue !=="") {
+		heading.innerText =`${headingValue}?year=${yearInputValue}`;
+	}
+    else if (nameInputValue !="" && yearInputValue =="") {
+		heading.innerText =`${headingValue}?name=${nameInputValue}`;
+	}
+    else{
+        heading.innerText =`${headingValue}?name=${nameInputValue}&year=${yearInputValue}`;
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
